b15e25f12205cbf834c75ed672a0fb6ee6542cd2
Derrick A Chan <dachanman91@gmail.com>
2017-05-13 22:46:02 -0700

    Start avoidObstacles.js

---
+++ avoidObstacles.js
@@ -0,0 +1,16 @@
+function avoidObstacles(inputArray) {
+  var obstacles = {};
+  var increment = 2;
+  if (inputArray.length === 0) {
+    return 1;
+  } else {
+    var current = 0
+    while (current <= 40)
+  }
+  for (var i = 0; i < inputArray.length; i++) {
+    obstacles[inputArray[i]] = true;
+  }
+  for (var j = 0; j < inputArray.length; j += increment) {
+
+  }
+}
